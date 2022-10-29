db43c4a1ef18af2fb2777c1de93325040a064e78
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-29 23:11:15 +0000

    J-2 条件分岐

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-// 足し算
-alert(4 + 3);
+let orange = 100;
+let apple = 120;
 
-// 引き算
-alert(8 - 5);
-
-// 掛け算
-alert(2 * 6);
-
-// 割り算
-alert(10 / 2);
-
-alert('Hello' + 'World');
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else {
+  alert('みかんの値段がりんごより高い');
+}
