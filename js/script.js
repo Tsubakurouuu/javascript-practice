af715de1792c64e66d46287676e19fad69e96c13
Tsubakurouuu <yzpolice2896@icloud.com>
2022-11-01 01:58:00 +0000

    J-12 Ajaxを実装する 天気情報をリクエストする処理を追記する

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+// (API_KEYには、"取得したAPIキーを記述")
+const API_KEY = "06d9dc741ee670076cd0fd6300c8eab8";
 $(function(){
   $('#btn').on('click', function(){
-    
-  })
-})
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType: 'jsonp',
+    })
+  });
+});
